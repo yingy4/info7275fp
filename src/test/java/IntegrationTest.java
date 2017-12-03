7e52bc3d145499501caff5d724ba85ea52205d6d
yingy4 <yingy4@hotmail.com>
2017-12-03 14:15:40 +0800

    Add more assertions in tests

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -4,13 +4,29 @@ import topk.PatentTopKClassificationChain;
 import org.apache.hadoop.fs.Path;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 class IntegrationTest {
 
     @Test
     void runPatentCountingByDate() throws Exception {
         Path input = new Path("input/testinput/rs2016.csv");
         Path outputDir = new Path("output/testoutput1");
-        Assertions.assertEquals(0, PatentCountingByDate.run(input,outputDir), "PatentCountingByDate failed");
+        Assertions.assertEquals(0, PatentCountingByDate.run(input,outputDir), "PatentCountingByDate failed!");
+        File f = new File("output/testoutput1/part-r-00000");
+        FileInputStream fis = new FileInputStream(f);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        String line = null;
+        int count = 0;
+        while ((line = br.readLine()) != null) {
+            count++;
+        }
+        br.close();
+        Assertions.assertEquals(288, count, "Total count not match!");
     }
 
     @Test
@@ -26,7 +42,30 @@ class IntegrationTest {
         Path temp1 = new Path("output/temp/ptkc");
         Path temp2 = new Path("output/temp/ptkc2");
         int k = Integer.parseInt(ins[3]);
-        Assertions.assertEquals(0, PatentTopKClassificationChain.run(input,input2,temp1,temp2,outputDir,k), "PatentTopKClassificationChain failed");
+        Assertions.assertEquals(0, PatentTopKClassificationChain.run(input,input2,temp1,temp2,outputDir,k), "PatentTopKClassificationChain failed!");
+
+        ArrayList<String> result = new ArrayList<String>();
+        result.add("257	Active solid-state devices (e.g., transistors, solid-state diodes),114");
+        result.add("370	Multiplex communications,110");
+        result.add("514	Drug, bio-affecting and body treating compositions,89");
+        result.add("424	Drug, bio-affecting and body treating compositions,77");
+        result.add("435	Chemistry: molecular biology and microbiology,70");
+        result.add("455	Telecommunications,57");
+        result.add("428	Stock material or miscellaneous articles,54");
+        result.add("705	Data processing: financial, business practice, management, or cost/price determination,50");
+        result.add("709	Electrical computers and digital processing systems: multicomputer data transferring,48");
+        result.add("438	Semiconductor device manufacturing: process,45");
+
+        File f = new File("output/testoutput2/part-m-00000");
+        FileInputStream fis = new FileInputStream(f);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        String line = null;
+        int count = 0;
+        while ((line = br.readLine()) != null) {
+            Assertions.assertEquals(result.get(count), line, "Result line " + count + " not right!");
+            count++;
+        }
+        br.close();
     }
 
 }
